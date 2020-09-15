fbfee6fdf90479b0458ac43580c391a0f5786ba3
JorgeDirkx <jorge.dirkx@gmail.com>
2020-09-15 21:56:07 +0200

    refactoring Test void foo() + update pom.xml

--- src/test/java/com/jorgedirkx/GildedRoseTest.java
+++ src/test/java/com/jorgedirkx/GildedRoseTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GildedRoseTest {
 
+   //test 1:
     @Test
     void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
